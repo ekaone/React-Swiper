46f2adf5f3e9389427bad06fcf636f8a80f27b5e
ekaone <ekaone3033@gmail.com>
2019-12-16 13:40:44 +0700

    Updates

---
+++ precache-manifest.d8f6a40cfe7f7b6eec6b65e560c8dc00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2c57ecbb3ea1161afcb32bca1a1e91f",
+    "url": "/swiperreact/index.html"
+  },
+  {
+    "revision": "ec298fc147454e6cb860",
+    "url": "/swiperreact/static/js/2.01be47d9.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/swiperreact/static/js/2.01be47d9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "05450b10e4a1cef1de7d",
+    "url": "/swiperreact/static/js/main.6686485c.chunk.js"
+  },
+  {
+    "revision": "18e47e56fd5f14a006b5",
+    "url": "/swiperreact/static/js/runtime-main.d4c8d5d6.js"
+  }
+]);
